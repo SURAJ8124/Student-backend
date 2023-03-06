8ce7970774d17bd5d66a7423011dace473ef88e4
SURAJ8124 <surajchavan1532@gmail.com>
2023-03-06 14:42:48 +0530

    Second Commit

--- src/main/java/com/example/demo/controller/studentcontroller.java
+++ src/main/java/com/example/demo/controller/studentcontroller.java
@@ -1,13 +1,15 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.example.demo.studentrepository.studentrepository;
 import com.example.demo.model.student;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -21,6 +23,12 @@ public class studentcontroller {
         System.out.println("Student input is " + student);
         studentrepository.save(student);
         return "Student Created in database";
+    }
+    @GetMapping("/students")
+    public ResponseEntity<List<student>>getAllstudents(){
+        List<student> studList= new ArrayList<>();
+        studentrepository.findAll().forEach(studList::add );
+        return new ResponseEntity<List<student>>(studList, HttpStatus.OK);
     }
 
 }
